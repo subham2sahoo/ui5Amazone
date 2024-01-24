149dfb632a0965ad8b019faeb943ed8ac1bfddb8
SUBHAM SAHOO <suvm1sahoo@gmail.com>
2024-01-24 12:59:11 +0000

    day2

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,18 +6,24 @@ sap.ui.define([
      */
     function (Controller) {
         "use strict";
-
+        var that;
         return Controller.extend("a.controller.View1", {
             onInit: function () {
+                that = this;
 
             },
             onCollapseExpandPress: function () {
                 var oSideNavigation = this.byId("sideNavigation");
                 var bExpanded = oSideNavigation.getVisible();
-    
+
                 oSideNavigation.setVisible(!bExpanded);
+                if(!bExpanded){
+                    that.byId("all").addStyleClass("select");
+                }else{
+                    that.byId("all").removeStyleClass("select");
+                }
             },
-    
+
             onHideShowSubItemPress: function () {
                 var oNavListItem = this.byId("subItem3");
                 oNavListItem.setVisible(!oNavListItem.getVisible());
